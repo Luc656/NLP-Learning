aac953d3e8c3c7d1e045dfb8510c50e9d4d25831
Luc656 <luc@daviesboys.co.uk>
2025-03-12 17:34:42 +0000

    adding new model and lemmatisation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
 nltk.download("stopwords")
+nltk.download('wordnet')
+nltk.download('punkt')
 
